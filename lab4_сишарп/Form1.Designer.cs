e09c8c162b6c9844778d90f5fe484795c769a14f
Лера Богданова <lerochka.bogdanova.01@mail.ru>
2020-11-02 22:17:07 +0900

    ДЗ

--- lab4_сишарп/Form1.Designer.cs
+++ lab4_сишарп/Form1.Designer.cs
@@ -39,6 +39,13 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -66,7 +73,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(83, 24);
+            this.textBox2.Location = new System.Drawing.Point(2, 25);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(163, 20);
             this.textBox2.TabIndex = 2;
@@ -74,7 +81,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(171, 50);
+            this.button2.Location = new System.Drawing.Point(90, 125);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
@@ -85,7 +92,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(80, 8);
+            this.label1.Location = new System.Drawing.Point(-1, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(145, 13);
             this.label1.TabIndex = 4;
@@ -103,7 +110,7 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(83, 136);
+            this.listBox1.Location = new System.Drawing.Point(2, 176);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(120, 95);
             this.listBox1.TabIndex = 6;
@@ -111,16 +118,16 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(319, 25);
+            this.textBox3.Location = new System.Drawing.Point(319, 176);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(110, 20);
             this.textBox3.TabIndex = 7;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(316, 9);
+            this.label3.Location = new System.Drawing.Point(316, 157);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(82, 13);
             this.label3.TabIndex = 8;
@@ -129,17 +136,86 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(80, 110);
+            this.label4.Location = new System.Drawing.Point(3, 157);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(109, 13);
             this.label4.TabIndex = 9;
             this.label4.Text = "Результаты поиска:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(2, 63);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(163, 20);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(-1, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(149, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Максимальное расстояние:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(-1, 86);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(113, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Количество потоков:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(2, 102);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(163, 20);
+            this.textBox5.TabIndex = 13;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(171, 176);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(168, 157);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Диапазоны:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(319, 218);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 53);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Сохранить результаты в файл";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(778, 283);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox3);
@@ -170,6 +246,13 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- lab4_сишарп/Form1.cs
+++ lab4_сишарп/Form1.cs
@@ -8,13 +8,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.IO;
 
 namespace lab4_сишарп
 {
     public partial class Form1 : Form
     {
 
-        List<string> split_text = new List<string>();
+        public static List<string> split_text = new List<string>();
+        public static Stopwatch timer = new Stopwatch();
 
         public Form1()
         {
@@ -75,10 +78,271 @@ namespace lab4_сишарп
 
         }
 
+
+
+        /// <summary>
+        /// Вычисление расстояния Дамерау-Левенштейна
+        /// </summary>
+        public static int Distance(string str1Param, string str2Param)
+        {
+            if ((str1Param == null) || (str2Param == null)) return -1;
+            int str1Len = str1Param.Length;
+            int str2Len = str2Param.Length;
+            //Если хотя бы одна строка пустая,
+            //возвращается длина другой строки
+            if ((str1Len == 0) && (str2Len == 0)) return 0;
+            if (str1Len == 0) return str2Len;
+            if (str2Len == 0) return str1Len;
+            //Приведение строк к верхнему регистру
+            string str1 = str1Param.ToUpper();
+            string str2 = str2Param.ToUpper();
+            //Объявление матрицы
+            int[,] matrix = new int[str1Len + 1, str2Len + 1];
+            //Инициализация нулевой строки и нулевого столбца матрицы
+            for (int i = 0; i <= str1Len; i++) matrix[i, 0] = i;
+
+            for (int j = 0; j <= str2Len; j++) matrix[0, j] = j;
+            //Вычисление расстояния Дамерау-Левенштейна
+            for (int i = 1; i <= str1Len; i++)
+            {
+                for (int j = 1; j <= str2Len; j++)
+                {
+                    //Эквивалентность символов, переменная symbEqual
+                    //соответствует m(s1[i],s2[j])
+                    int symbEqual = (
+                    (str1.Substring(i - 1, 1) ==
+                    str2.Substring(j - 1, 1)) ? 0 : 1);
+                    int ins = matrix[i, j - 1] + 1; //Добавление
+                    int del = matrix[i - 1, j] + 1; //Удаление
+                    int subst = matrix[i - 1, j - 1] + symbEqual; //Замена
+                                                                  //Элемент матрицы вычисляется
+                                                                  //как минимальный из трех случаев
+                    matrix[i, j] = Math.Min(Math.Min(ins, del), subst);
+                    //Дополнение Дамерау по перестановке соседних символов
+                    if ((i > 1) && (j > 1) &&
+                    (str1.Substring(i - 1, 1) == str2.Substring(j - 2, 1)) &&
+                    (str1.Substring(i - 2, 1) == str2.Substring(j - 1, 1)))
+                    {
+                        matrix[i, j] = Math.Min(matrix[i, j],
+                        matrix[i - 2, j - 2] + symbEqual);
+                    }
+                }
+            }
+            //Возвращается нижний правый элемент матрицы
+            return matrix[str1Len, str2Len];
+        }
+
+
+        /// <summary>
+        /// Вывод расстояния Дамерау-Левенштейна в консоль
+        /// </summary>
+        public static void WriteDistance(string str1Param, string str2Param)
+        {
+            int d = Distance(str1Param, str2Param);
+            Console.WriteLine("'" + str1Param + "','" +
+            str2Param + "' -> " + d.ToString());
+        }
+
+
+
+        /// <summary>
+        /// Хранение минимального и максимального значений диапазона
+        /// </summary>
+        public class MinMax
+        {
+            public int Min { get; set; }
+            public int Max { get; set; }
+
+            public MinMax(int pmin, int pmax)
+            {
+                this.Min = pmin;
+                this.Max = pmax;
+            }
+        }
+
+
+
+        /// <summary>
+        /// Класс для деления массива на последовательности
+        /// </summary>
+        public static class SubArrays
+        {
+            /// <summary>
+            /// Деление массива на последовательности
+            /// </summary>
+            /// <param name="beginIndex">Начальный индекс массива</param>
+            /// <param name="endIndex">Конечный индекс массива</param>
+            /// <param name="subArraysCount">Требуемое количество подмассивов</param>
+            /// <returns>Список пар с индексами подмассивов</returns>
+            public static List<MinMax> DivideSubArrays(int beginIndex, int endIndex, int subArraysCount)
+            {
+                //Результирующий список пар с индексами подмассивов
+                List<MinMax> result = new List<MinMax>();
+
+                //Если число элементов в массиве слишком мало для деления 
+                //то возвращается массив целиком
+                if ((endIndex - beginIndex) <= subArraysCount)
+                {
+                    result.Add(new MinMax(0, (endIndex - beginIndex)));
+                }
+                else
+                {
+                    //Размер подмассива
+                    int delta = (endIndex - beginIndex) / subArraysCount;
+                    //Начало отсчета
+                    int currentBegin = beginIndex;
+                    //Пока размер подмассива укладывается в оставшуюся последовательность
+                    while ((endIndex - currentBegin) >= 2 * delta)
+                    {
+                        //Формируем подмассив на основе начала последовательности
+                        result.Add(new MinMax(currentBegin, currentBegin + delta));
+                        //Сдвигаем начало последовательности вперед на размер подмассива
+                        currentBegin += delta;
+                    }
+                    //Оставшийся фрагмент массива
+                    result.Add(new MinMax(currentBegin, endIndex));
+                }
+                //Возврат списка результатов
+                return result;
+            }
+
+        }
+
+
+        /// <summary>
+        /// Выполняется в параллельном потоке для поиска слова
+        /// </summary>
+        /// <param name="param"></param>
+        public static List<string> ArrayThreadTask(object paramObj)
+        {
+            
+            //Получение параметров
+            Tuple<List<string>, string, int> param = (Tuple<List<string>, string, int>)paramObj;
+            int listCount = param.Item1.Count;
+            
+            //Временный список для результата
+            List<string> tempData = new List<string>();
+
+            //Перебор нужных элементов в списке данных
+            for (int i = 0; i < listCount; i++)
+            {
+                //Текущее значение из массива
+                string temp = param.Item1[i];
+                int dist = Distance(temp.ToLower(), param.Item2);
+                if (dist <= param.Item3)
+                    tempData.Add(temp);
+
+            }
+
+                
+            //Возврат массива данных
+            return tempData;
+        }
+
+
+
+
+
+
+
+        /// <summary>
+        /// Многопоточный поиск в массиве
+        /// </summary>
+        public  List<string> ArrayThreadExample(int ArrayLength, int ThreadCount, string word, int max_dist)
+        {
+            //Результирующий список чисел 
+            List<string> Result = new List<string>();
+
+            //Создание и заполнение временного списка данных
+            List<string> tempList = new List<string>();
+            foreach (string str in split_text)
+                tempList.Add(str);
+                
+
+            //Деление списка на фрагменты для параллельного запуска в потоках
+            List<MinMax> arrayDivList = SubArrays.DivideSubArrays(0, ArrayLength, ThreadCount);
+            int count = arrayDivList.Count;
+
+
+            
+
+            //Очистка списка
+            this.listBox2.Items.Clear();
+            //Вывод диапазонов деления исходного массива
+            for (int i = 0; i < count; i++)
+            {
+                //Вывод результатов, найденных в каждом потоке
+                this.listBox2.Items.Add("Диапазон " + i.ToString() + ": " + arrayDivList[i].Min + " - " + arrayDivList[i].Max);
+            }
+            Console.WriteLine();
+
+            //Обновление таймера
+            timer.Reset();
+            //Запуск таймера
+            timer.Start();
+
+            //Количество потоков соответствует количеству фрагментов массива
+            Task<List<string>>[] tasks = new Task<List<string>>[count];
+
+            //Запуск потоков
+            for (int i = 0; i < count; i++)
+            {
+                //Создание временного списка, чтобы потоки не работали параллельно с одной коллекцией
+                List<string> tempTaskList = tempList.GetRange(arrayDivList[i].Min, arrayDivList[i].Max - arrayDivList[i].Min);
+
+                tasks[i] = new Task<List<string>>(
+                    //Метод, который будет выполняться в потоке
+                    ArrayThreadTask,
+                    //Параметры потока передаются в виде кортежа, чтобы не создавать временный класс
+                    new Tuple<List<string>, string, int>(tempTaskList, word, max_dist));
+
+                //Запуск потока
+                tasks[i].Start();
+            }
+            
+            //Ожидание завершения всех потоков
+            Task.WaitAll(tasks);
+
+            //Остановка таймера
+            timer.Stop();
+
+            //Объединение результатов полученных из разных потоков
+            for (int i = 0; i < count; i++)
+            {
+                //Вывод результатов, найденных в каждом потоке
+                Console.Write("Поток " + i.ToString() + ": ");
+                foreach (var x in tasks[i].Result) Console.Write(x.ToString() + " ");
+                Console.WriteLine();
+
+                //Добавление результатов конкретного потока в общий массив результатов
+                Result.AddRange(tasks[i].Result);
+            }
+
+            //Вывод общего массива результатов
+            
+
+            return Result;
+
+            /*
+            Console.WriteLine("\nМассив из {0} элементов обработан {1} потоками за {2}. Найдено: {3}", ArrayLength, count, timer.Elapsed, Result.Count);
+            foreach (string i in Result) Console.Write(i.PadRight(5));
+            Console.WriteLine();*/
+        }
+
+
+
+
+
+
+
+
         private void button2_Click(object sender, EventArgs e)
         {
             //определяем слово для поиска в нижнем регистре
             string word = this.textBox2.Text.Trim().ToLower();
+            int max_dist = int.Parse(this.textBox4.Text);
+            int potok_kol = int.Parse(this.textBox5.Text);
+
 
             if (string.IsNullOrWhiteSpace(word) || split_text.Count == 0)
             {
@@ -86,21 +350,32 @@ namespace lab4_сишарп
                 return;
             }
 
-            //Временные результаты поиска
             List<string> find_list = new List<string>();
+
+            /*
+            //Временные результаты поиска
+            
 
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
 
+        
+
             foreach (string str in split_text)
             {
-                if (str.ToLower().Contains(word))
-                {
+                int dist = Distance(str.ToLower(), word);
+                if (dist<=max_dist)
                     find_list.Add(str);
-                }
             }
+
+
             stopWatch.Stop();
-            this.textBox3.Text = stopWatch.Elapsed.ToString();
+
+    */
+
+
+            find_list = ArrayThreadExample(split_text.Count, potok_kol, word, max_dist);
+            this.textBox3.Text = timer.Elapsed.ToString();
 
             this.listBox1.BeginUpdate();
 
@@ -113,8 +388,9 @@ namespace lab4_сишарп
                 this.listBox1.Items.Add(str);
             }
             this.listBox1.EndUpdate();
+            
 
-
+            
         }
 
         private void textBox3_TextChanged(object sender, EventArgs e)
@@ -125,6 +401,102 @@ namespace lab4_сишарп
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Имя файла отчета
+            string TempReportFileName = "Report_" + DateTime.Now.ToString("dd_MM_yyyy_hhmmss");
+
+            //Диалог сохранения файла отчета
+            SaveFileDialog fd = new SaveFileDialog();
+            fd.FileName = TempReportFileName;
+            fd.DefaultExt = ".html";
+            fd.Filter = "HTML Reports|*.html";
+
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                string ReportFileName = fd.FileName;
+
+                //Формирование отчета
+                StringBuilder b = new StringBuilder();
+                b.AppendLine("<html>");
+
+                b.AppendLine("<head>");
+                b.AppendLine("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'/>");
+                b.AppendLine("<title>" + "Отчет: " + ReportFileName + "</title>");
+                b.AppendLine("</head>");
+
+                b.AppendLine("<body>");
+
+                b.AppendLine("<h1>" + "Отчет: " + ReportFileName + "</h1>");
+                b.AppendLine("<table border='1'>");
+
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Время чтения из файла</td>");
+                b.AppendLine("<td>" + this.textBox1.Text + "</td>");
+                b.AppendLine("</tr>");
+
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Количество уникальных слов в файле</td>");
+                b.AppendLine("<td>" + split_text.Count + "</td>");
+                b.AppendLine("</tr>");
+
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Слово для поиска</td>");
+                b.AppendLine("<td>" + this.textBox2.Text + "</td>");
+                b.AppendLine("</tr>");
+
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Максимальное расстояние для нечеткого поиска</td>");
+                b.AppendLine("<td>" + this.textBox4.Text + "</td>");
+                b.AppendLine("</tr>");
+
+
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Время нечеткого поиска</td>");
+                b.AppendLine("<td>" + this.textBox3.Text + "</td>");
+                b.AppendLine("</tr>");
+
+                b.AppendLine("<tr valign='top'>");
+                b.AppendLine("<td>Результаты поиска</td>");
+                b.AppendLine("<td>");
+                b.AppendLine("<ul>");
+
+                foreach (var x in this.listBox1.Items)
+                {
+                    b.AppendLine("<li>" + x.ToString() + "</li>");
+                }
+
+                b.AppendLine("</ul>");
+                b.AppendLine("</td>");
+                b.AppendLine("</tr>");
+
+                b.AppendLine("</table>");
+
+                b.AppendLine("</body>");
+                b.AppendLine("</html>");
+                
+                //Сохранение файла
+                File.AppendAllText(ReportFileName, b.ToString());
+
+                MessageBox.Show("Отчет сформирован. Файл: " + ReportFileName);
+            }
         }
     }
 }
